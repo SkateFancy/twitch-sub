88e397c91bf5b0a5a3aa4109031a09f0a7bfcde1
Felix <felixletsplay@outlook.de>
2018-04-25 18:01:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $database = "sub";
 $dbusername = "root";
 $dbpassword = "";
 //ClientID of Twitch OAuth Application
-$clientId = '0e3j4no7rk76ja8ibzkghumfnh110g';
+$clientId = 'Your Twitch ClientID';
 //Channel where user sub should be checked
 $channelSub = 'skate702';
 //your own website address. should be the same like in oauth settings
